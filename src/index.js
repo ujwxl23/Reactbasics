c9ae3dd841070c2d6c169ffe65b627acb6107286
UJWAL KUMAR <89293303+ujwxl23@users.noreply.github.com>
2023-05-05 16:07:53 +0530

    Adding css
    css for grid structure and styling

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'; //To add components
 import ReactDOM from 'react-dom';
+import './index.css';
 
 
 //JSX Rules
@@ -21,18 +22,18 @@ import ReactDOM from 'react-dom';
 // }
 
 //METHOD-2
-function Greeting(){ 
-  return (
-  <div>
-    <Person /> 
-    <Message />
-  </div>
-  ); //Here, we are calling the components made in const.
-}
-const Person = () => <h4>This is React Project</h4>; //Value in const 
-const Message = () => {
-  return <p>By Ujwal Kumar</p>;
-};
+// function Greeting(){ 
+//   return (
+//   <div>
+//     <Person/>
+//     <Message/>
+//   </div>
+//   ); //Here, we are calling the components made in const.
+// }
+// const Person = () => <h4>This is React Project</h4>; //Value in const 
+// const Message = () => {
+//   return <p>By Ujwal Kumar</p>;
+// };
 
 
 //METHOD-3
@@ -40,4 +41,38 @@ const Message = () => {
 //   return React.createElement('div',{},React.createElement('h1',{},"hello world")); //Same as above html return function 
 // }
 
-ReactDOM.render(<Greeting/>,document.getElementById('root')); // Must contain a closing tag.... //Contains two arguments  what to render and the where to render it in html file (div id)
+
+function BookList(){
+  return(
+    <section className='booklist'>
+      <Book/>
+      <Book/>
+      <Book/>
+    </section>
+  );
+}
+
+const Book = () =>{
+  return (
+  <article>
+    <BookImage/>
+    <BookTitle/>
+    <BookAuthor/>
+  </article>
+  );
+  };
+
+const BookImage = () => ( 
+  <img src='https://images-eu.ssl-images-amazon.com/images/I/71B4h-dSVzL._AC_UL600_SR600,400_.jpg' alt=''/>
+  );
+
+const BookTitle = () => (
+  <h1>Energize Your Mind: Learn the Art of Mastering your Mind</h1>
+);
+
+const BookAuthor =() => (
+  <h4>Gaur Gopal Das</h4>
+)
+
+
+ReactDOM.render(<BookList/>,document.getElementById('root')); // Must contain a closing tag.... //Contains two arguments  what to render and the where to render it in html file (div id)
